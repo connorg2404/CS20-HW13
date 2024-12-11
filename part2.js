11c274611397ab6b5a035a5be8da20b29d2ed67b
connorg2404 <157325793+connorg2404@users.noreply.github.com>
2024-12-10 23:23:34 -0500

    Add files via upload

--- part2.js
+++ part2.js
@@ -9,10 +9,6 @@ var port = process.env.PORT || 3000;
 const uri = 'mongodb+srv://connorg2404:Tusd2026@cs20-hw13.be1nl.mongodb.net/?retryWrites=true&w=majority&appName=CS20-HW13';
 
 http.createServer(async function (req, res) {
-    const client = new MongoClient(uri);
-    client.connect();
-    const db = client.db('Stock');
-    const collection = db.collection('PublicCompanies');
     res.writeHead(200, { 'Content-Type': 'text/html' });
     var urlObj = url.parse(req.url, true);
     var path = urlObj.pathname;
@@ -34,7 +30,12 @@ http.createServer(async function (req, res) {
         var searchType = query.type;
 
         // Connect to MongoDB and search
+        const client = new MongoClient(uri);
         try {
+            await client.connect();
+            const db = client.db('Stock');
+            const collection = db.collection('PublicCompanies');
+
             // Query the database based on search type (case-insensitive)
             let dbQuery = {};
             if (searchType === 'ticker') {
@@ -44,7 +45,7 @@ http.createServer(async function (req, res) {
             }
 
             // Get and display results
-            const results = collection.find(dbQuery).toArray();
+            const results = await collection.find(dbQuery).toArray();
             res.write('<h1>Search Results</h1>');
             if (results.length === 0) {
                 res.write('<p>No results found.</p>');
